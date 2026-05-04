24182f3be02936714cc0f10be817921e1cfa6c2c
xpertdigital <89961924+xpertdigital@users.noreply.github.com>
2026-05-04 08:13:23 +0530

    Update and rename vite.config.ts to app.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@tanstack/start/config'
-import { cloudflareAdapter } from '@tanstack/start-adapter-cloudflare'
-
-export default defineConfig({
-  server: {
-    preset: 'cloudflare-pages',
-  },
-})
